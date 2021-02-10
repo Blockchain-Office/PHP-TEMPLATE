30e0cab5c506ac320b4c9541368f21abae5a4b11
Blockchain Office <71699794+Blockchain-Office@users.noreply.github.com>
2021-02-10 12:12:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,26 @@
 <?php
 
-	$FILEBLOCK[0] = [
+$FILEBLOCK[0] = [
   'html.php'
-  ];
+ ];
 
-	$DBQD[0] = [
-	'{#TITLE#}' 			=> 'Simple HTML/PHP - TEMPLATE/PARSER - PHP EVAL HACKS',
-	'{#CONTENT#}' 		=> 'Simple HTML/PHP TEMPLATE'
-	];
+$DBQD[0] = [
+'{#TITLE#}' 		=> 'Simple HTML/PHP - TEMPLATE/PARSER - PHP EVAL HACKS',
+'{#CONTENT#}' 		=> 'Simple HTML/PHP TEMPLATE'
+];
 
-	$FILE = array();
-	foreach($FILEBLOCK[0] as $HTML_OUT){
-		array_push($FILE, file_get_contents($HTML_OUT));
-	}
+$FILE = array();
+foreach($FILEBLOCK[0] as $HTML_OUT){
+	array_push($FILE, file_get_contents($HTML_OUT));
+}
 
-	$EVALBOX ='';
-	foreach($FILE as $OUTPUT){
-		$EVALBOX = $EVALBOX.(preg_replace(array_keys($DBQD[0]), $DBQD[0], $OUTPUT));
-	}
+$EVALBOX ='';
+foreach($FILE as $OUTPUT){
+	$EVALBOX = $EVALBOX.(preg_replace(array_keys($DBQD[0]), $DBQD[0], $OUTPUT));
+}
 
-	eval(" ?> $EVALBOX <?php ");
+eval(" ?> $EVALBOX <?php ");
 
-	exit;
+exit;
 
 ?>
